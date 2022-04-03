1547ef25aa32d7c645a2010e0a028b7ff816d791
Abhinavjain123 <abhinavsra@gmail.com>
2022-04-03 18:23:21 +0530

    Added 22multi_dimen_arr.c

---
+++ 22multi_dimen_arr.c
@@ -0,0 +1,18 @@
+/*
+array can be of 2 dimention/3 dimention \ n dimention
+
+*/
+#include<stdio.h> 
+
+int main()
+{
+    int arr[3][2]={ {1,4},
+                    {7,9},
+                    {11,33}
+    };
+    // we can access the elements by arr[1][1]
+    printf("%d",arr[1][1]);
+    
+
+    return 0;
+}
